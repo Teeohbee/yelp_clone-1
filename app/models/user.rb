40ee6ab870650f58adb1e91554a191ba6ce3063c
Retesh <retesh.bajaj@gmail.com>
2015-09-04 09:26:44 +0100

    for Toby

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = auth.info.email || "#{auth.uid}@facebook.com"
       user.password = Devise.friendly_token[0,20]
       user.provider = auth.provider
       user.uid = auth.uid
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,54 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
+  context 'after signing in' do
+    before(:each) do
+      user = User.create(email: "test3@test.com", password: "testpass", provider: "facebook", uid: 12345)
+      visit '/'
+      click_link 'Sign in'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button "Log in"
+    end
+
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+
+    scenario 'let a user edit a restaurant' do
+      Restaurant.create(name:'KFC')
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      click_button 'Update Restaurant'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(current_path).to eq '/restaurants'
+    end
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      Restaurant.create(name:'KFC')
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+
+  end
+
+    context "not signed in" do
+      scenario 'user must be logged in to create a restaurant' do
+        visit '/'
+        click_link 'Add a restaurant'
+        expect(Restaurant.count).to be(0)
+        expect(page).to have_content('You need to sign in or sign up before continuing')
+      end
+    end
+
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
@@ -20,20 +68,9 @@ feature 'restaurants' do
       end
     end
   end
-  context 'creating restaurants' do
-    scenario 'prompts user to fill out a form, then displays the new restaurant' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      expect(page).to have_content 'KFC'
-      expect(current_path).to eq '/restaurants'
-    end
-  end
+
   context 'viewing restaurants' do
-
     let!(:kfc){Restaurant.create(name:'KFC')}
-
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'KFC'
@@ -41,28 +78,5 @@ feature 'restaurants' do
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
-  context 'editing restaurants' do
 
-    before {Restaurant.create name: 'KFC'}
-
-    scenario 'let a user edit a restaurant' do
-      visit '/restaurants'
-      click_link 'Edit KFC'
-      fill_in 'Name', with: 'Kentucky Fried Chicken'
-      click_button 'Update Restaurant'
-      expect(page).to have_content 'Kentucky Fried Chicken'
-      expect(current_path).to eq '/restaurants'
-    end
-  end
-  context 'deleteing restaurants' do
-
-    before {Restaurant.create name: 'KFC'}
-
-    scenario 'removes a restaurant when a user clicks a delete link' do
-      visit '/restaurants'
-      click_link 'Delete KFC'
-      expect(page).not_to have_content 'KFC'
-      expect(page).to have_content 'Restaurant deleted successfully'
-    end
-  end
 end
