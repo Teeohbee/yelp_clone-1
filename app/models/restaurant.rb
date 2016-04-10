dba402c1deaaaa4421f0339c4612df0d3d09e346
Toby Clarke <teeohbee@gmail.com>
2016-04-10 19:40:27 +0100

    Latest commits online

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
+  belongs_to :user
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
     it { should have_many(:reviews).dependent(:destroy) }
+
+    context 'relationship with users' do
+      it { is_expected.to belong_to :user }
+    end
 end
